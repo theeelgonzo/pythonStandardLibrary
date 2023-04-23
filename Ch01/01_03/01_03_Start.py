a368d2b6ed392fca189032dae4f2db78092d67c0
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-23 05:59:20 -0500

    Add length code.

--- Ch01/01_03/01_03_Start.py
+++ Ch01/01_03/01_03_Start.py
@@ -1,3 +1,14 @@
 # Calculating Length
 
 # len() --> returns length
+firstName = 'Taylor'
+print(len(firstName))
+lastName = 'Swift'
+print(len(lastName))
+
+ages = [0, 11, 43, 12, 10]
+print(len(ages))
+
+i = 0
+for i in range(0, len(ages)):
+    print(ages[i])
