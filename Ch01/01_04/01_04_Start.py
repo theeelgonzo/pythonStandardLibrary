bb0d772bf5b9ff3247f9df32a9528935053256ca
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-23 06:03:19 -0500

    Add code to range, list files.

--- Ch01/01_04/01_04_Start.py
+++ Ch01/01_04/01_04_Start.py
@@ -1,2 +1,10 @@
 # Range -> range instance that holds all nums counting by one between 0 and first input
 # List -> lists numbers from the inputted tuple
+numberedContestants = range(30)
+print(list(numberedContestants))
+
+for c in list(numberedContestants):
+    print("contestant " + str(c) + ' is here')
+
+luckyWinners = range (7, 30, 5)
+print(list(luckyWinners))
