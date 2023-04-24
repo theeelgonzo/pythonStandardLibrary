7cce27243048ad8d929702052bbb7aa217973e42
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-24 12:54:55 -0500

    Add sorted function file.

--- Ch01/01_07/01_07_Start.py
+++ Ch01/01_07/01_07_Start.py
@@ -1,6 +1,17 @@
 # Least to Greatest
-
+pointsInAGame = [0, -10, 15, -2, 1, 12]
+sortedGame = sorted(pointsInAGame)
+print(sortedGame)
 # Alphabetically
+children = ['sue', 'jerry', 'linda']
+print(sorted(children))
 
 # Key Parameters
+print(sorted("My favorite child is Linda".split(), key=str.upper))
+print(sorted(pointsInAGame, reverse=True))
 
+leaderBoard = {231: 'CKL', 123: 'ABC', 432: 'JKC'}
+print(sorted(leaderBoard, reverse=True))
+
+students = [('Alice', 'B', 12), ('Eliza', 'A', 16), ('Tae', 'C', 15)]
+print(sorted(students, key=lambda student:student[1]))
