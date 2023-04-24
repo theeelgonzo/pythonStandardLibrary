ca9490b9d752e90b8013537dc69d7f8a68bd17d8
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-24 12:47:04 -0500

    Add min max files, round and abs file.

--- Ch01/01_05/01_05_Start.py
+++ Ch01/01_05/01_05_Start.py
@@ -1,1 +1,10 @@
 # Minimum and Maximum
+playerOneScore = 10
+playerTwoScore = 4
+print(min(playerOneScore, playerTwoScore))
+print(min(0, 12, -19))
+
+print(max(playerOneScore, playerTwoScore))
+playerThreeScore = 14
+print(max(playerThreeScore, playerTwoScore, playerOneScore))
+
--- Ch01/01_06/01_06_Start.py
+++ Ch01/01_06/01_06_Start.py
@@ -1,7 +1,23 @@
 # Python Rounding, Absolute Value, and Exponents
 
 # round()
+myGPA = 3.6
+print(round(myGPA))
+amountOfSalt = 1.4
+print(round(amountOfSalt))
+
+apple = -1.2
+print(round(apple))
+google = -1.6
+print(round(google))
 
 # abs()
+distanceAway = -13
+print(abs(distanceAway))
+lengthOfRoot = -2.5
+print(abs(lengthOfRoot))
 
 # pow()
+chanceOfTails = 0.5
+inARowTail = 3
+print(pow(chanceOfTails, inARowTail))
