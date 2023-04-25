6738b81b3267f1199ac5361bf595207db23220ec
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-25 13:33:15 -0500

    add math functions

--- Ch02/02_01/02_01_Start.py
+++ Ch02/02_01/02_01_Start.py
@@ -1,7 +1,24 @@
 # Math Module Part I
+import math
 
 # Constants
+print(math.pi)
+print(math.e)
+print(math.nan)
+print(math.infinity)
 
 # Trigonometry
+obst_direction = math.cos(math.pi /4)
+print(obst_direction)
 
 # Ceiling and Floor
+cookies = 10.3
+candy = 7
+print(math.ceil(cookies))
+print(math.ceil(candy))
+age = 47.9
+print(math.floor(age))
+otherAge = 47
+print(math.floor(otherAge))
+
+
