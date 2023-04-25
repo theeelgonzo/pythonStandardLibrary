a9eaee3bf877c0f997bef03fbb0d9d979211fdd2
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-25 13:42:43 -0500

    Add code for random module.

--- Ch02/02_03/02_03_Start.py
+++ Ch02/02_03/02_03_Start.py
@@ -1,5 +1,18 @@
 # Random Module
-
+import random
 # Random Numbers
+print(random.random())
+decider = random.randrange(2)
+if decider == 0:
+    print('heads')
+else:
+    print('tails')
+print(decider)
 
+print(random.randrange(1, 7))
 # Random Choices
+lotteryWinners = random.sample(range(100), 5)
+print(lotteryWinners)
+
+possiblePets = ['cat', 'dog', 'fish']
+print(random.choice(possiblePets))
