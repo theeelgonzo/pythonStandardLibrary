2d95ae3f846b7906b64fddf77955bb89f0258a51
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-26 21:19:04 -0500

    add command line input file

--- Ch03/03_01/03_01_Start.py
+++ Ch03/03_01/03_01_Start.py
@@ -1,7 +1,18 @@
 # Command Line Arguments
-
+import sys
 # Print Arguments
-
+print('number of arguments: ', len(sys.argv), ' arguments')
+print('arguments', sys.argv)
 # Remove Arguments
-
+sys.argv.remove(sys.argv[0])
+print('arguments', sys.argv)
 # Sum up the Arguments
+arguments = sys.argv
+sum = 0
+for arg in arguments:
+    try:
+        number = int(arg)
+        sum = sum + number
+
+    except Exception:
+        print('bad input')
