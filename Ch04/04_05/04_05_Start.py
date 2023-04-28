2e9a10b410e794ab22126666577ceaf4f93be9c1
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 11:20:10 -0500

    Add html parsing code.

--- Ch04/04_05/04_05_Start.py
+++ Ch04/04_05/04_05_Start.py
@@ -1,1 +1,20 @@
 # HTML Parser Module
+from html.parser import HTMLParser
+
+class HTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('Start tag: ', tag)
+        for attr in attrs:
+            print('attr: ', attr)
+        
+    def handle_endtag(self, tag):
+        print('end tag: ', tag)
+    
+    def handle_comment(self, data):
+        print('comment: ', data)
+    def handle_data(self, data):
+        print('data: ', data)
+
+parser = HTMLParser()
+parser.feed('<html><head><title>Coder</title></head><body><h1><!--hi-->I am a coder</hi></body></html>')
+print()
