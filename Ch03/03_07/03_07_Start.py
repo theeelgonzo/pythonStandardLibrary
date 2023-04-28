9b8f4d0e0a2a52c1736bed4b1ba3a4189e65897e
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 10:51:24 -0500

    add zip manipulation code

--- Ch03/03_07/03_07_Start.py
+++ Ch03/03_07/03_07_Start.py
@@ -1,7 +1,8 @@
 # Zipfile Module
-
+import zipfile
 # Open and List
-
+zip = zipfile.ZipFile('Archive.zip', 'r')
+print(zip.namelist())
 # Metadata in the zip folder
 
 # Access to files in zip folder
