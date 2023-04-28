c39a7c324c4be0af2c759db99ba318bb56d8140a
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 10:58:53 -0500

    Add datetime module code.

--- Ch04/04_01/04_01_Start.py
+++ Ch04/04_01/04_01_Start.py
@@ -1,1 +1,17 @@
 # Datetime Module Part I
+from datetime import datetime
+
+now = datetime.now()
+print(now.date())
+
+print(now.year)
+
+print(now.month)
+
+print(now.hour)
+
+print(now.minute)
+
+print(now.second)
+
+print(now.time())
