3acec257e93dfc5ebf6a75d31352559ac4b0c65e
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 11:02:46 -0500

    Add datetime formatting code.

--- Ch04/04_02/04_02_Start.py
+++ Ch04/04_02/04_02_Start.py
@@ -2,3 +2,11 @@
 from datetime import datetime
 
 now = datetime.now()
+
+print(now.strftime('%a %A %d'))
+
+print(now.strftime('%b %B %m'))
+
+print(now.strftime('%a %B %d'))
+
+print(now.strftime('%H %M %S %p'))
