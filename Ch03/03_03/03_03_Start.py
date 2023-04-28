20bad7a881f3b6fc5897c29ba3c4b69adbed6bd8
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-27 20:25:47 -0500

    Add file output text.

--- Ch03/03_03/03_03_Start.py
+++ Ch03/03_03/03_03_Start.py
@@ -1,13 +1,19 @@
 # Files and File Writing
 
 # Open a file
-
+myFile = open('scores.txt', 'w')
 # w --> write
 # r --> read
 # r+ --> read and write
 # a --> append
 # Show attributes and properties of that file
-
+print(' name ' + myFile.name
+)
+print('mode ' + myFile.mode)
 # Write to a file
+myFile.write('GBJ : 100 \nKHD: 99\nBBB: 89')
+myFile.close()
 
 # Read the file
+myFile = open('scores.txt', 'r')
+print('reading...' + myFile.read(10))
