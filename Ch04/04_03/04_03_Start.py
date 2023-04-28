7e2e3acb2cbbc37cc7608113bafc9feaa3fc67b0
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 11:09:52 -0500

    Add calendar module code.

--- Ch04/04_03/04_03_Start.py
+++ Ch04/04_03/04_03_Start.py
@@ -1,4 +1,13 @@
 # Calendar Module
-from datetime import datetime
+from datetime import datetime, timedelta
+import calendar
 
 now = datetime.now()
+
+testDate = now + timedelta(days = 2)
+myFirstLinkedInCourse = now - timedelta(weeks = 3)
+print(testDate.date())
+print(myFirstLinkedInCourse.date())
+
+cal = calendar.month(2001, 10)
+print(cal)
