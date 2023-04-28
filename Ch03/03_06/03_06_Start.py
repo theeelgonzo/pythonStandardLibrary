4582492a8eb4327283beaa6aa20cb6886a7396b7
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 10:46:58 -0500

    Add temp file code.

--- Ch03/03_06/03_06_Start.py
+++ Ch03/03_06/03_06_Start.py
@@ -1,9 +1,12 @@
 # Tempfile Module
-
+import tempfile
 # Create a temporary file
-
+tempFile = tempfile.TemporaryFile()
 # Write to a temporary file
+tempFile.write(b'save this special number for me: 5678309')
+tempFile.seek(0)
 
 # Read the temporary file
-
+print(tempFile.read())
 # Close the temporary file
+tempFile.close()
