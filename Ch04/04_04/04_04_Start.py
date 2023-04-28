c479aea906f21747596c78a7ae1d86753b94468e
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-28 11:12:28 -0500

    Create timer function.

--- Ch04/04_04/04_04_Start.py
+++ Ch04/04_04/04_04_Start.py
@@ -1,1 +1,11 @@
 # Create a Timer with the Time module
+import time
+
+run = input('Start? >')
+
+seconds = 0
+if run == 'yes':
+    while seconds != 10:
+        print('>', seconds)
+        time.sleep(1)
+        seconds += 1
